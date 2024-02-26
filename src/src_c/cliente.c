7b49dd92d1c05c4423bb1fa3f9799eb582a7ceec
barjimen <barby.jimenez17@gmail.com>
2024-02-27 00:25:40 +0100

    changes

--- src/src_c/cliente.c
+++ src/src_c/cliente.c
@@ -6,13 +6,23 @@
 /*   By: barjimen <barjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 20:43:07 by barjimen          #+#    #+#             */
-/*   Updated: 2024/02/20 20:29:28 by barjimen         ###   ########.fr       */
+/*   Updated: 2024/02/27 00:23:44 by barjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minitalk.h"
 
-
+static void signal_handler(int signo, siginfo_t *info, void *n)
+{
+    (void) info;
+    (void) n;
+    
+    if(signo == SIGUSR1)
+        ft_putendl_fd("Recibido.", 1);
+    else if(signo == SIGUSR2)
+        ft_putendl_fd("Ocupado, pruebe más tarde.",1);
+    exit(0);
+}
 void    manda_char(int pid, char ch)
 {
     int i;
@@ -35,20 +45,28 @@ void    manda_char(int pid, char ch)
 
 int main(int argc,char **argv)
 {
+    struct sigaction client;
+    
     int pid;
     int longi;
     int count;
     
+    client.sa_flags = SA_SIGINFO;
+    client.sa_sigaction = signal_handler;
+    sigaction(SIGUSR1,&client, NULL);
+    sigaction(SIGUSR2,&client, NULL);
     count = 0;
     (void)argc;
     pid = ft_atoi(argv[1]);
     longi = ft_strlen(*argv);
-    while (longi != 0)
+    while (argv[2][count])
     {
         manda_char(pid, argv[2][count]);
         longi--;
         count++;
     }
-    //manda_char(pid, '\n');
+    manda_char(pid, '\n');
+    //manda_char(pid, '\0');
+
     return(0);
 }
--- src/src_s/servidor.c
+++ src/src_s/servidor.c
@@ -6,29 +6,55 @@
 /*   By: barjimen <barjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 20:43:10 by barjimen          #+#    #+#             */
-/*   Updated: 2024/02/20 20:44:46 by barjimen         ###   ########.fr       */
+/*   Updated: 2024/02/27 00:10:11 by barjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minitalk.h"
-void signal_handler(int signo)
+static void signal_handler(int signo, siginfo_t *info, void *n)
 {
+	static char chr;
+	static int count;
+	static pid_t pid_c;
+	
+	if(pid_c == 0)
+		pid_c = info->si_pid;
+	(void)n;
+	if(pid_c != info->si_pid)
+	{
+		kill(info->si_pid, SIGUSR2);
+		return ;
+	}
 	if (signo == SIGUSR1)
-		ft_putstr_fd("1", 1);
-	if (signo == SIGUSR2)
-		ft_putstr_fd("0", 1);
+		chr = chr | 128 >> count;
+	count++;
+	if (count == 8)
+	{
+		write(1, &chr, 1);
+		count = 0;
+		chr = 0;
+		if (chr == '\0')
+		{
+			kill(info->si_pid, SIGUSR1);
+			pid_c = 0;
+		}
+	}
 }
 int	main(void)
 {
+	struct sigaction serv;
+	
 	char *numb;
 	pid_t pid;
-
+	
+	serv.sa_flags = SA_SIGINFO;
+	serv.sa_sigaction = signal_handler;
 	pid = getpid();
 	numb = ft_itoa(pid);
 	ft_putstr_fd("El pid es: ", 1);
 	ft_putendl_fd(numb, 1);
-	signal(SIGUSR1, signal_handler);
-	signal(SIGUSR2, signal_handler);
+	sigaction(SIGUSR1, &serv, NULL);
+	sigaction(SIGUSR2, &serv, NULL);
 
 	while (1)
 		;
